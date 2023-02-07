0989b59f0fe7de95e6d4e89c1c8023780dbc8bc3
pierre-adrienm <pierre-adrien.marque@institutsolacroup.com>
2023-02-07 15:46:20 +0100

    07022023 V6

--- views/admin/tags/form.php
+++ views/admin/tags/form.php
@@ -13,16 +13,11 @@ function submitPostLink()
 // var_dump($_REQUEST);
 ?>
 
-<form method="POST" name="postlink" action="<?= isset($params['tags']) ? HREF_ROOT."admin/posts/edit/{$params['tags']->id}" :  "../../admin/tags/create" ?>" >
+<form method="POST" name="tagslink" action="<?= isset($params['tags']) ? HREF_ROOT."admin/tags/edit/{$params['tags']->name}" :  "../../admin/tags/create" ?>" >
     <div class="form-group">
-        <label for="title">Titre de l'article</label>
-        <input type="text" class="form-control" name="title" id="title" value="<?= $params['tags']->title ?? '' ?>">
+        <label for="title">Nom</label>
+        <input type="text" class="form-control" name="name" id="name" value="<?= $params['tags']->name ?? '' ?>">
     </div>
-    <div class="form-group">
-        <label for="content">Contenu de l'article</label>
-        <textarea name="content" id="content" rows="8" class="form-control"><?= $params['tags']->content ?? '' ?></textarea>
-    </div>
-
     
     <button type="submit"  class="btn btn-primary"><?= isset($params['tags']) ? "Enregistrer les modifications" : "Enregistrer mon article" ?></button>
     
