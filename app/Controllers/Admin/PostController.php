5a5dbe01588f88823b8fb189bcc41b5a84be4b73
pierre-adrienm <pierre-adrien.marque@institutsolacroup.com>
2023-02-13 12:09:25 +0100

    13022023 V3

--- app/Controllers/Admin/PostController.php
+++ app/Controllers/Admin/PostController.php
@@ -14,6 +14,7 @@ class PostController extends Controller {
      */
     public function index()
     {
+        // verifirer si on est connectée en tant qu'administrateur
         $this->isAdmin();
 
         $posts = (new Post($this->getDB()))->all();
@@ -27,6 +28,7 @@ class PostController extends Controller {
      */
     public function create()
     {
+        // verifirer si on est connectée en tant qu'administrateur
         $this->isAdmin();
 
         $tags = (new Tag($this->getDB()))->all();
@@ -40,8 +42,10 @@ class PostController extends Controller {
      */
     public function createPost()
     {
+        // verifirer si on est connectée en tant qu'administrateur
         $this->isAdmin();
 
+        // créer un nouveau post
         $post = new Post($this->getDB());
 
         $tags = array_pop($_POST);
@@ -61,13 +65,16 @@ class PostController extends Controller {
     public function edit(int $id)
     {
        // var_dump("Model edit:", $id);
+       // verifirer si on est connectée en tant qu'administrateur
         $this->isAdmin();
 
+        // créer une nouvelle instant de post et de tag
         $post = (new Post($this->getDB()))->findById($id);
         $tags = (new Tag($this->getDB()))->all();
         
         //var_dump("Model edit:", $post);
         //var_dump("Model edit:", $tags);
+        // retourner le résulta de la vue de post et de tags
         return $this->view('admin.post.form', compact('post', 'tags'));
         //return $this->view('admin.post.form', compact('post'));
     }
@@ -79,16 +86,21 @@ class PostController extends Controller {
      */
     public function update(int $id)
     {
+        // verifirer si on est connectée en tant qu'administrateur
         $this->isAdmin();
 
+        // créer une nouvelle instance de post
         $post = new Post($this->getDB());
 
         // var_dump("PostController update:",$_POST);
+        // retourne de déplie la dernière valeur de $_POST et attribue à la variable $tags
         $tags = array_pop($_POST);
        // var_dump("PostController update:",$_POST, $tags);
 
+        // modifie le post à modifier et attribue à la variable $post
         $result = $post->update($id, $_POST, $tags);
 
+        // si c'est modifier retourne le résultat sur la page 'admin/posts'
         if ($result) {
             return header('Location: '.HREF_ROOT.'admin/posts');
         }
@@ -101,11 +113,14 @@ class PostController extends Controller {
      */
     public function destroy(int $id)
     {
+        // verifirer si on est connectée en tant qu'administrateur
         $this->isAdmin();
 
+        // créer une nouvelle instant de Post puis attribué le post à détruire sur la variable $post
         $post = new Post($this->getDB());
         $result = $post->destroy($id);
 
+        // si c'est supprimé retourne le résultat sur la page 'admin/post'
         if ($result) {
             return header('Location: '.HREF_ROOT.'admin/posts');
         }
--- app/Controllers/Admin/TagController.php
+++ app/Controllers/Admin/TagController.php
@@ -11,10 +11,13 @@ class TagController extends Controller{
 
     public function index()
     {
+        // verifirer si on est connectée en tant qu'administrateur
         $this->isAdmin();
 
+        // créer un nouveau instance tag et tous indexer
         $tags = (new Tag($this->getDB()))->all();
 
+        // retourne le résultat de l'affiche sur 'admin.tags.index'
         return $this->view('admin.tags.index', compact('tags'));
     }
 
@@ -24,10 +27,13 @@ class TagController extends Controller{
      */
     public function create()
     {
+        // verifirer si on est connectée en tant qu'administrateur
         $this->isAdmin();
 
+        // créer un nouveau instance tag et tous indexer
         $tags = (new Tag($this->getDB()))->all();
 
+        // retourne le résultat de l'affiche sur 'admin.tags.form'
         return $this->view('admin.tags.form');
     }
 
@@ -37,14 +43,18 @@ class TagController extends Controller{
      */
     public function createTag()
     {
+        // verifirer si on est connectée en tant qu'administrateur
         $this->isAdmin();
 
+        // créer un nouveau instance de tag
         $tag = new Tag($this->getDB());
 
         // $tags = array_pop($_POST);
 
+        // récréer un tag est l'attribué sur la variable $result
         $result = $tag->create($_POST);
 
+        // si le tag est créer retourner le résultat sur 'admin/tags'
         if ($result) {
             return header('Location:'.HREF_ROOT.'admin/tags');
         }
@@ -58,12 +68,15 @@ class TagController extends Controller{
     public function edit(int $id)
     {
        // var_dump("Model edit:", $id);
-        $this->isAdmin();
+       // verifirer si on est connectée en tant qu'administrateur 
+       $this->isAdmin();
 
+        // créer un nouveau instance de tag est retourver pour un id
         $tags = (new Tag($this->getDB()))->findById($id);
         
         //var_dump("Model edit:", $post);
         //var_dump("Model edit:", $tags);
+        // retourner le vue des tags sur 'admin.tags.form'
         return $this->view('admin.tags.form', compact('tags'));
     }
 
@@ -74,15 +87,19 @@ class TagController extends Controller{
      */
     public function update(int $id)
     {
+        // verifirer si on est connectée en tant qu'administrateur
         $this->isAdmin();
 
+        // créer un nouveau instant de tag
         $tags = new Tag($this->getDB());
 
         // var_dump("PostController update:",$_POST);
        // var_dump("PostController update:",$_POST, $tags);
 
+        // mondifier le tags est attribée à la variable $result
         $result = $tags->update($id, $_POST);
 
+        // si le tags est modifier retourner le résultat sur 'admin/tags'
         if ($result) {
             return header('Location: '.HREF_ROOT.'admin/tags');
         }
@@ -95,11 +112,15 @@ class TagController extends Controller{
      */
     public function destroy(int $id)
     {
+        // verifirer si on est connectée en tant qu'administrateur
         $this->isAdmin();
 
+        // créer un nouveau instance tags
         $tag = new Tag($this->getDB());
+        // supprimer le tags est attribué à la variable $result
         $result = $tag->destroy($id);
 
+        // si le tags est supprimer retourner le résultat sur 'admin/tags"
         if ($result) {
             return header('Location: '.HREF_ROOT.'admin/tags');
         }
