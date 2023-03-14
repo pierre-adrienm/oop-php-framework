0016f957dd556ee2d9596e57b7b01435994cd444
pierre-adrienm <pierre-adrien.marque@institutsolarcroup.com>
2023-03-14 10:42:32 +0100

    14032023 V2

--- views/blog/show.php
+++ views/blog/show.php
@@ -3,6 +3,10 @@
     <?php foreach ($params['post']->getTags() as $tag) : ?>
         <span class="badge badge-info"><?= $tag->name ?></span>
     <?php endforeach ?>
+    <?php foreach ($params['post']->getMedias() as $media) : ?>
+        <span><img class="img-thumbnail" src="../public/<?= $media->path ?>"></span>
+        <!-- <span class="badge badge-info"><?= $media->path ?></span> -->
+    <?php endforeach ?>
 </div>
 <p><?= $params['post']->content ?></p>
 <a href="<?= HREF_ROOT ?>posts" class="btn btn-secondary">Retourner en arrière</a>
